7928d5182a2716849b1e1a7a89980cb96f47cede
z548668 <dheepak.kumar@one.verizon.com>
2015-06-05 14:59:53 -0400

    updated code

--- src/main/java/com/test/config/Application.java
+++ src/main/java/com/test/config/Application.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	 }
+	 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
