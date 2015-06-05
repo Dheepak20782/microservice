8abf60099b66583cd35cc30f0a2b35ae76c2f886
z548668 <dheepak.kumar@one.verizon.com>
2015-06-05 15:10:10 -0400

    updated code

--- src/main/java/com/test/config/Application.java
+++ src/main/java/com/test/config/Application.java
@@ -3,12 +3,18 @@ package com.test.config;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan({"com.test.controller"})
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
 	
